5920a88a3dee34f868f737a8142aaa857d5919d0
Julian Turner <julian@trner.de>
2023-03-13 19:56:08 +0000

    setup ubunut devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,2 @@
+sudo apt-get update
+sudo apt-get install build-essential gdb cmake -y
--- 0_Playground/Simple_Calc.cpp
+++ 0_Playground/Simple_Calc.cpp
@@ -3,4 +3,5 @@
 int main() {
     int result = 1 + 1;
     std::cout << "The result is: " << result << std::endl;
+    return 0;
 }
